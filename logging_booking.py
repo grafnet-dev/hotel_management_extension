bbb707315ecb31dda892c342cd29fb2c47101cb7
Sedjro <manwenselove@gmail.com>
2025-09-18 18:49:51 +0100

    feat:regle tariffaire pour le type flexible ajustée

---
+++ logging_booking.py
@@ -0,0 +1,25 @@
+# logging_booking.py
+import logging
+import os
+
+LOG_DIR = "."   # à adapter, par exemple "/var/log/odoo"
+LOG_FILE = os.path.join(LOG_DIR, "booking.log")
+
+# Création du répertoire si inexistant
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# Récupération d’un logger spécifique
+booking_logger = logging.getLogger("hotel.booking")
+booking_logger.setLevel(logging.DEBUG)
+
+# Création d’un FileHandler dédié
+fh = logging.FileHandler(LOG_FILE)
+fh.setLevel(logging.DEBUG)
+
+# Format des logs
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
+fh.setFormatter(formatter)
+
+# Éviter d’ajouter plusieurs handlers si déjà configuré
+if not booking_logger.handlers:
+    booking_logger.addHandler(fh)
